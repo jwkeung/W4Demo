addbff63a9a972445f110164b7f51321422e5027
sliu273 <sliu273@uct01.cs.cityu.edu.hk>
2024-09-27 10:40:16 +0800

    Student 2 : made changes to the Q2_2 test case, removing the old version stub_1 and kept the stub_v2

--- M1/src/testDaysMonthV1/TestDaysMonthV1.java
+++ M1/src/testDaysMonthV1/TestDaysMonthV1.java
@@ -61,18 +61,8 @@ public class TestDaysMonthV1 {
 	
 	@Test
 	public void testQ2_2(){
-		class StubLeapYearV1 extends LeapYearV1 {
-			//Overwrite the class functions
-			public boolean isLeapYear(int year){
-				boolean leap;
-				//Write your own code here to manipulate the variable leap.
-				if(year == 2004)//This is an example of test sub.
-					leap = true;
-				else
-					leap = false;
-				return leap;
-			}
-		}
+		
+		
 		class StubLeapYearV2 extends LeapYearV1 {
 			public boolean isLeapYear(int year) {
 				return false;
